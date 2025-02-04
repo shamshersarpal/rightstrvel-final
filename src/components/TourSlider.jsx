a92565f34dab847313317fdd87cfc0599444169b
shamshersarpal <shamshersingh01567@gmail.com>
2025-02-04 11:33:36 +0530

    update

--- src/components/TourSlider.jsx
+++ src/components/TourSlider.jsx
@@ -1,130 +1,122 @@
-"use client";
-import React, { useState, useEffect } from "react";
-import SearchBar from "@/components/SearchBar";
-
-export default function HeroSection() {
-  const videos = [
-    "/videos/videoone.mp4",
-    "/videos/videotwo.mp4",
-    "/videos/videothree.mp4",
-  ];
-
-  const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
-  const [nextVideoIndex, setNextVideoIndex] = useState(1);
-  const [isFading, setIsFading] = useState(false);
-  const [isOpen, setIsOpen] = useState(false);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setIsFading(true);
-      setTimeout(() => {
-        setCurrentVideoIndex(nextVideoIndex);
-        setNextVideoIndex((prev) => (prev + 1) % videos.length);
-        setIsFading(false);
-      }, 800);  
-    }, 10000);
-
-    return () => clearInterval(interval);
-  }, [nextVideoIndex]);
-
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+  } from "@/components/ui/carousel";
+  import TourOne from "@/images/c1.jpg";
+  import TourTwo from "@/images/c2.jpg";
+  import TourThree from "@/images/c3.jpg";
+  import TourFour from "@/images/c4.jpg";
+  import TourFive from "@/images/c5.jpg"; 
+  import Image from "next/image"; 
+  export default function TourSlider() {
   return (
-    <main>
-      <div className="relative overflow-hidden h-screen">
-        
-        <video
-          key={videos[currentVideoIndex]}
-          className={`absolute inset-0 w-full h-full object-cover transition-opacity duration-800 ${
-            isFading ? "opacity-0" : "opacity-100"
-          }`}
-          src={videos[currentVideoIndex]}
-          type="video/mp4"
-          autoPlay
-          muted
-          loop
-        />
-
-      
-        <video
-          key={videos[nextVideoIndex]}
-          className={`absolute inset-0 w-full h-full object-cover transition-opacity duration-800 ${
-            isFading ? "opacity-100" : "opacity-0"
-          }`}
-          src={videos[nextVideoIndex]}
-          type="video/mp4"
-          autoPlay
-          muted
-          loop
-        />
-
-        <div className="py-60 bg-cover bg-center">
-          <div className="absolute inset-0 bg-black opacity-50"></div>
-          <div className="relative z-10 flex items-center justify-center h-full text-white">
-            <div className="px-4 grid md:grid-cols-12 grid-cols-1 w-full max-w-screen-xl items-center mt-10 gap-[30px]">
-              <div className="lg:col-span-8 md:col-span-7 md:order-1 order-2">
-                <h5 className="text-3xl font-dancing text-white courve">
-                  Tours &amp; Travel
-                </h5>
-                <h4 className="font-bold text-white text-4xl lg:text-6xl mb-6 mt-5">
-                  Discover Amazing <br />
-                  Places With Us
-                </h4>
-                <p className="text-white text-xl max-w-xl">
-                  Lorem ipsum is placeholder text commonly used in the graphic,
-                  print, and publishing industries for previewing layouts and
-                  visual mockups!
-                </p>
-              </div>
-              <div className="lg:col-span-4 md:col-span-5 md:text-center md:order-2 order-1">
-                <button
-                  onClick={() => setIsOpen(true)}
-                  className="lg:h-24 h-20 lg:w-24 w-20 rounded-full shadow-lg dark:shadow-gray-800 inline-flex items-center justify-center bg-white hover:bg-blue text-blue hover:text-white duration-500 ease-in-out mx-auto"
-                >
-                  <svg
-                    className="text-blue-700 ps-1"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width={44}
-                    height={44}
-                    fill="currentColor"
-                    viewBox="0 0 24 24"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M8.6 5.2A1 1 0 0 0 7 6v12a1 1 0 0 0 1.6.8l8-6a1 1 0 0 0 0-1.6l-8-6Z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                </button>
-              </div>
-            </div>
-          </div>
+  <>
+  <div className="container py-20 px-4">
+     <div className="max-w-screen-xl   mx-auto">
+        <div className="flex flex-wrap items-center justify-between">
         </div>
-
-        
-        {isOpen && (
-          <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-95 z-50">
-            <button
-              onClick={() => setIsOpen(false)}
-              className="absolute top-3 right-5 text-white text-xl"
-            >
-              ✕
-            </button>
-            <div className="relative bg-white rounded-lg shadow-lg w-full max-w-3xl">
-              <div className="relative w-full pt-[56.25%]">
-                <iframe
-                  className="absolute top-0 left-0 w-full h-full"
-                  src="https://www.youtube.com/embed/tO-ZUJTd62Q?autoplay=1"
-                  title="YouTube video"
-                  frameBorder="0"
-                  allow="autoplay; encrypted-media"
-                  allowFullScreen
-                ></iframe>
+        <Carousel
+        className="max-w-screen-xl mx-auto "
+        opts={{
+        align: "start",
+        }}
+        >
+        <div className="pb-10 pt-10 text-center">
+           <h2 className="  md:text-3xl text-2xl md:leading-normal leading-normal font-semibold">Top Destinations</h2>
+           <p className="text-slate-400 my-5 max-w-xl mx-auto">Planning for a trip? We will organize your trip with the best places and within best budget!</p>
+        </div>
+        <CarouselContent>
+           <CarouselItem   className="md:basis-1/2 lg:basis-1/5">
+              <div className=" ">
+                 <div className="group relative overflow-hidden rounded-md shadow dark:shadow-gray-800 m-2">
+                    <Image src={TourOne} className="w-full h-72 object-cover scale-125 group-hover:scale-100 duration-500"  alt=""/>
+                    <div className="absolute inset-0 bg-gradient-to-b to-slate-900 from-transparent opacity-0 group-hover:opacity-100 duration-500"></div>
+                    <div className="absolute p-4 bottom-0 start-0">
+                       <a
+                          href=""
+                          className="text-lg font-medium text-white hover:text-blue duration-500 ease-in-out"
+                          >
+                       Africa tours
+                       </a>
+                    </div>
+                 </div>
               </div>
-            </div>
-          </div>
-        )}
-      </div>
-      <SearchBar />
-    </main>
+           </CarouselItem>
+           <CarouselItem   className="md:basis-1/2 lg:basis-1/5">
+              <div className=" ">
+                 <div className="group relative overflow-hidden rounded-md shadow dark:shadow-gray-800 m-2">
+                    <Image src={TourTwo} className="w-full h-72 object-cover scale-125 group-hover:scale-100 duration-500"  alt=""/>
+                    <div className="absolute inset-0 bg-gradient-to-b to-slate-900 from-transparent opacity-0 group-hover:opacity-100 duration-500"></div>
+                    <div className="absolute p-4 bottom-0 start-0">
+                       <a
+                          href=""
+                          className="text-lg font-medium text-white hover:text-blue duration-500 ease-in-out"
+                          >
+                       Europe tours
+                       </a>
+                    </div>
+                 </div>
+              </div>
+           </CarouselItem>
+           <CarouselItem   className="md:basis-1/2 lg:basis-1/5">
+              <div className=" ">
+                 <div className="group relative overflow-hidden rounded-md shadow dark:shadow-gray-800 m-2">
+                    <Image src={TourThree} className="w-full h-72 object-cover scale-125 group-hover:scale-100 duration-500"  alt=""/>
+                    <div className="absolute inset-0 bg-gradient-to-b to-slate-900 from-transparent opacity-0 group-hover:opacity-100 duration-500"></div>
+                    <div className="absolute p-4 bottom-0 start-0">
+                       <a
+                          href=""
+                          className="text-lg font-medium text-white hover:text-blue duration-500 ease-in-out"
+                          >
+                       Europe tours
+                       </a>
+                    </div>
+                 </div>
+              </div>
+           </CarouselItem>
+           <CarouselItem   className="md:basis-1/2 lg:basis-1/5">
+              <div className=" ">
+                 <div className="group relative overflow-hidden rounded-md shadow dark:shadow-gray-800 m-2">
+                    <Image src={TourFour} className="w-full h-72 object-cover scale-125 group-hover:scale-100 duration-500"  alt=""/>
+                    <div className="absolute inset-0 bg-gradient-to-b to-slate-900 from-transparent opacity-0 group-hover:opacity-100 duration-500"></div>
+                    <div className="absolute p-4 bottom-0 start-0">
+                       <a
+                          href=""
+                          className="text-lg font-medium text-white hover:text-blue duration-500 ease-in-out"
+                          >
+                       Middle East tours
+                       </a>
+                    </div>
+                 </div>
+              </div>
+           </CarouselItem>
+           <CarouselItem   className="md:basis-1/2 lg:basis-1/5">
+              <div className=" ">
+                 <div className="group relative overflow-hidden rounded-md shadow dark:shadow-gray-800 m-2">
+                    <Image src={TourFive} className="w-full h-72 object-cover scale-125 group-hover:scale-100 duration-500"  alt=""/>
+                    <div className="absolute inset-0 bg-gradient-to-b to-slate-900 from-transparent opacity-0 group-hover:opacity-100 duration-500"></div>
+                    <div className="absolute p-4 bottom-0 start-0">
+                       <a
+                          href=""
+                          className="text-lg font-medium text-white hover:text-blue duration-500 ease-in-out"
+                          >
+                       America tours
+                       </a>
+                    </div>
+                 </div>
+              </div>
+           </CarouselItem>
+        </CarouselContent>
+        <div className="block lg:hidden sliderbtnMob">
+           <CarouselPrevious className=""  />
+           <CarouselNext />
+        </div>
+        </Carousel>
+     </div>
+  </div>
+  </>
   );
-}
+  }
